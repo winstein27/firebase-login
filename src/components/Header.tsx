a3ffcbe958d6964ffb41f6eb40a58646d3c29ff3
Winstein Martins <sriwinstein@gmail.com>
2023-04-02 07:06:19 -0300

    :sparkles: feat: adding icons
    Adding react-icons to the project and adding an icon to the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
+import {
+  MdDarkMode as DarkModeIcon,
+  //   MdOutlineDarkMode as DarkModeOutlinedIcon,
+} from 'react-icons/md';
+
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div>
-        <span className={styles.mode}>Dark mode</span>
+        <span className={styles.mode}>
+          <DarkModeIcon />
+        </span>
         <nav className={styles.navbar}>
           <ul className={styles.navlinks}>
             <li>LOG IN</li>
